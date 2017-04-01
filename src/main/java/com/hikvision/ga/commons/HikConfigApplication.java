322056224dedb0e8dedca17953bacbbc85065558
wuqi <wuqi5@hikvision.com.cn>
2017-04-01 15:16:34 +0800

    初始化项目hik-config

---
+++ src/main/java/com/hikvision/ga/commons/HikConfigApplication.java
@@ -0,0 +1,12 @@
+package com.hikvision.ga.commons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikConfigApplication.class, args);
+	}
+}
